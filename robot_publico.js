0d887c487df3c5cbd80ac89a4eed0f1d9af1bce0
laurentum <32101926+laurentum@users.noreply.github.com>
2019-08-12 01:05:46 -0400

    Update robot_publico.js

--- robot_publico.js
+++ robot_publico.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Robot DLV
 // @version      1.52b
-// @description  Cambiaron el titulo y se corrigió la función tiemporestante
+// @description  Se ha eliminado dependencia de jQuery
 // @author       laurentum
 // @match        https://freebitco.in/*
 // @grant        none
@@ -9,7 +9,7 @@
 // @downloadURL  https://raw.githubusercontent.com/laurentum/chorrito/master/robot_publico.js
 // ==/UserScript==
 
-(setTimeout(function() {
+(function() {
 	'use strict';
     var version="1.52b";
 
@@ -26,57 +26,62 @@
 		tR.tiempo = 0;
 	    return(tR.tiempo);
 	  }
-        // función para reportar
-        function Reportar(estatus) {
-          estatus=encodeURIComponent(estatus);
-          var parametros="Id="+userID+"&Btc="+balance_BTC+"&Rp="+balance_PR+"&Status="+estatus+"&Version="+version;
-          peticion=$.ajax({
-            crossDomain: true,	
-            url: "https://script.google.com/macros/s/AKfycbzrBiC5Of2eAGPxoLBVFqcQ6W9mTu0N9Y3b2JWCTLYoeZ2s6npG/exec?"+parametros,
-	    method : "GET",
-            dataType: "jsonp"
-          });
-	  // Callback handler en caso de éxito
-	  peticion.done(function (response, textStatus, jqXHR){
-	    // Pasa un mensaje a la consola
-	    if (response["fila"]!=-1) {			
-	      console.log("Escribiendo en la fila ",response["fila"]);
-	    } else {
-	      console.log("Maquina no está en mis registros");
-            }
-	  });
-	} // Reportar
+    // función para reportar
+    function Reportar(estatus) {
+      estatus=encodeURIComponent(estatus);
+      var parametros="Id="+userID+"&Btc="+balance_BTC+"&Rp="+balance_PR+"&Status="+estatus+"&Version="+version;
+      var peticion = new XMLHttpRequest();
+      peticion.open('GET', 'https://script.google.com/macros/s/AKfycbzrBiC5Of2eAGPxoLBVFqcQ6W9mTu0N9Y3b2JWCTLYoeZ2s6npG/exec?'+parametros, true);
+      peticion.onload = function() {
+        if (this.status >= 200 && this.status < 400) {
+          // Success!
+          var respuesta = this.response;
+    	    // Pasa un mensaje a la consola
+          if (respuesta["fila"]!=-1) {			
+            console.log("Escribiendo en la fila ",respuesta["fila"]);
+          } else {
+            console.log("Maquina no está en mis registros");
+          }
+        } else {
+          // We reached our target server, but it returned an error
+        }
+      };
+      peticion.onerror = function() {
+        // There was a connection error of some sort
+      };
+      peticion.send();
+  	} // Reportar
 
 	// Esta es la función principal que hace todo
 	// Se ejecuta 2 seg después esperando los resultados del rebote
 	
 	function accion_principal() {
+    ventanita=document.createElement('DIV');
+    ventanita.style["position"]="fixed";
+    ventanita.style["top"]="45px";
+    ventanita.style["left"]="0";
+    ventanita.style["z-index"]="999";
+    ventanita.style["width"]="300px";
+    ventanita.style["background-color"]=color_robot;
+    ventanita.style["color"]="#ffffff";
+    ventanita.style["text-align"]="left";
+    ventanita.id="autochorrito";
+    estilochorrito=document.createElement("STYLE");
+    estilochorrito.innerHTML="#autochorrito p { margin: 0; margin-left: 2px;  text-align: left; }";
+    document.body.appendChild(estilochorrito);
+    textoventanita="<p style='text-decoration:bold; text-align:center'>"+
+      "( ͡° ͜ʖ ͡°) ╭∩╮  v."+version+"<br />"+"───────────────────────</p>";
 		if (autorizado) {
-			body.prepend(
-				$('<div/>').attr('style',"position:fixed;top:45px;left:0;z-index:999;width:300px;background-color:"+color_robot+";color: white; text-align: left;")
-				.append(
-					$('<div/>').attr('id','autochorrito')
-					.append($('<p/>').attr('style','text-decoration:bold; text-align:center').text("( ͡° ͜ʖ ͡°) ╭∩╮  v."+version))
-					.append($('<p/>').attr('style','text-decoration:bold; text-align:center').text("───────────────────────"))
-					.append($('<p/>').text(userID))
-					.append($('<p/>').text(acct_email))
-					.append($('<p/>').text(estado))
-					.append($('<p/>').text(estado_captcha))
-				)
-			).prepend($('<style/>').text("#autochorrito p { margin: 0; margin-left: 2px;  text-align: left; }"));
+      textoventanita+="<p>"+userID+"<br />"+acct_email+"<br />"+estado+"<br />"+estado_captcha+"</p>";
 			// activa la rutina para que se ejecute repetidamente de manera asíncrona según condiciones:
 			if (!hay_captcha & !bloqueo_ip & !timer_running) {	
 				premios.rutina();  // reclama los premios de RP
 				var timeout=Math.floor(Math.random() * 60 )*1000+2000; //timeout adicional entre 2 y 62 segundos
-				if ($('#free_play_form_button').is(':visible')) {
+				if (document.getElementById('free_play_form_button').style.display!=='none') {
 					console.log("Cobrando el premio.");
-					setTimeout(function(){ $('#free_play_form_button').click();},timeout);
+					setTimeout(function(){document.getElementById('free_play_form_button').click();},timeout);
+          setTimeout(function(){document.querySelectorAll('.close-reveal-modal')[22].click();},timeout+12000); // cierra la ventana de dialogo pop-up 12 segundos despues de jugar el chorrito
 				}
-				setTimeout(function(){
-					if ($('.close-reveal-modal').is(':visible')) {
-						setTimeout(function(){$('.close-reveal-modal').click();},1000);
-					}
-				},timeout+12000); // cierra la ventana de dialogo pop-up 12 segundos despues de jugar el chorrito
 				setTimeout(function(){location.reload(true);},timeout+3610000); //obliga a hacer un refrescamiento de la pagina en una hora
 				Reportar(estatus_reporte); // manda el reporte cada hora
 			} else {
@@ -87,18 +92,11 @@
 				Reportar(estatus_reporte); // reportar cada hora de todas formas
 			}
 		} else { //si no está autorizado
-			body.prepend(
-				$('<div/>').attr('style',"position:fixed;top:45px;left:0;z-index:999;width:300px;background-color:#a40000; color: white; text-align: left;")
-				.append(
-					$('<div/>').attr('id','autochorrito')
-					.append($('<p/>').attr('style','text-decoration:bold; text-align:center').text("( ͡° ͜ʖ ͡°) ╭∩╮  v."+version))
-					.append($('<p/>').attr('style','text-decoration:bold; text-align:center').text("───────────────────────"))
-					.append($('<p/>').text("La cuenta "+userID+" no esta autorizada para usar este robot."))
-					.append($('<p/>').text("Por lo tanto, la paloma sea contigo."))
-
-				)
-			).prepend($('<style/>').text("#autochorrito p { margin: 0; margin-left: 2px;  text-align: left; }"));
+      textoventanita+="<p>La cuenta "+userID+" no esta autorizada para usar este robot."+
+        "Por lo tanto, la paloma sea contigo.</p>";
 		}
+    ventanita.innerHTML=textoventanita;
+    document.body.appendChild(ventanita);
 	} // accion_principal
 	  // premios es un objeto con una función para activar bonos para
 	  // acrecentar rápidamente los puntos reward y cobrar el bono
@@ -107,13 +105,14 @@
 	  var estatus_reporte = "Balance a la hora";
 	  premios.rutina = function() {
 		  var prob_bonos = 0;
-      premios.puntos = parseInt($('.user_reward_points').text().replace(',',""));
+      premios.puntos = parseInt(document.querySelectorAll('.user_reward_points')[0].innerHTML.replace(',',""));
       premios.temporizadorbono = {};
-      if ($("#bonus_container_free_points").length !== 0) {
-        premios.temporizadorbono.texto = $('#bonus_span_free_points').text();
+      if (document.getElementById("bonus_container_free_points")!== null) {
+        premios.temporizadorbono.texto = document.getElementById("bonus_span_free_points").innerHTML;
         premios.temporizadorbono.hora = parseInt(premios.temporizadorbono.texto.split(":")[0]);
         premios.temporizadorbono.minuto = parseInt(premios.temporizadorbono.texto.split(":")[1]);
         premios.temporizadorbono.segundo = parseInt(premios.temporizadorbono.texto.split(":")[2]);
+        estatus_reporte+=" ("premios.temporizadorbono.texto+")";
         premios.temporizadorbono.actual = premios.temporizadorbono.hora * 3600 + premios.temporizadorbono.minuto * 60 + premios.temporizadorbono.segundo;
       } else
         premios.temporizadorbono.actual = 0;
@@ -167,27 +166,26 @@
 		   }
 	   };
 
-	  var body = $('body');
 	  var autorizado=true;
 	  // datos de esta cuenta
 	  var userID = (((document.getElementById('edit_tab')).getElementsByTagName('p')[0]).getElementsByTagName('span')[1]).innerHTML;
 	  userID = parseInt(userID);
 	  var balance_BTC = parseFloat(document.getElementById('balance').innerHTML);
-	  var balance_PR = parseInt($('.user_reward_points').text().replace(',',""));
+	  var balance_PR = parseInt(document.querySelectorAll('.user_reward_points')[0].innerHTML.replace(',',""));
 	  var acct_email=document.getElementById('edit_profile_form_email').value;
 	  var estado="Estoy despierto.";
 	  // verifica si hay captcha u otras condiciones
-	  var hay_captcha=($('#captchasnet_free_play_captcha').is(':visible'))||($('#free_play_recaptcha').is(':visible'));
-	  var timer_running=($("#multi_acct_same_ip").is(":visible"));
-	  var bloqueo_ip=$('#free_play_error').is(':visible');
+	  var hay_captcha=(document.getElementById('captchasnet_free_play_captcha').style.display!=="none")||(document.getElementById('free_play_recaptcha').style.display!=="none");
+	  var timer_running=document.getElementById('multi_acct_same_ip').style.display!=="none";
+	  var bloqueo_ip=document.getElementById('free_play_error').style.display!="none";
 	  var estado_captcha="";
 	  var color_robot="#054908";
 	  if (hay_captcha) {estado_captcha="¡Fokin Captcha! Reportando a mi amo..."; color_robot="#a40000";}
 	  if (timer_running) {estado_captcha="El reloj está corriendo. Reportando a mi amo..."; color_robot="#a40000";}
 	if (!timer_running & !hay_captcha) {
 		estado_captcha="Voy a cobrar el chorrito";
-		if ($("#bonus_container_free_points").length !== 0) {estado_captcha+=".";}
+		if (document.getElementById("bonus_container_free_points")!== null) {estado_captcha+=".";}
 		else {estado_captcha+=" y también voy a activar bonos (si tengo suficientes RP).";}
 	}
  	setTimeout(accion_principal,2000); // espera 2 seg para ver si está autorizado o no y ejecuta el resto.
-},5000))();
+})();
